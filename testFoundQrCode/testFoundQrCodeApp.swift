2fca629d06b800d66a0717902c465374dd90025d
chenzhizsqq <chenzhi@qq.com>
2022-12-14 15:52:28 +0900

    Initial Commit

---
+++ testFoundQrCode/testFoundQrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testFoundQrCodeApp.swift
+//  testFoundQrCode
+//
+//  Created by chenzhizs on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct testFoundQrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
