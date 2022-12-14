15f54c079edc63385f28dd783add5c7808e0842e
chenzhizsqq <chenzhi@qq.com>
2022-12-14 16:34:49 +0900

    先例子那样，做完了一个二维码的swift了 https://qiita.com/ikaasamay/items/58d1a401e98673a96fd2

---
+++ testFoundQrCode/CameraPreview.swift
@@ -0,0 +1,36 @@
+//
+//  CameraPreview.swift
+//  testFoundQrCode
+//
+//  Created by chenzhizs on 2022/12/14.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreview: UIView {
+
+    private var label:UILabel?
+
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var session = AVCaptureSession()
+    weak var delegate: QrCodeCameraDelegate?
+
+    init(session: AVCaptureSession) {
+        super.init(frame: .zero)
+        self.session = session
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc func onClick(){
+        delegate?.onSimulateScanning()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        previewLayer?.frame = self.bounds
+    }
+}
--- testFoundQrCode/ContentView.swift
+++ testFoundQrCode/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ScannerViewModel()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack {
+            Text("QR Code Reader")
+                .padding()
+
+            // 読み取ったQRコード表示位置
+            Text("URL = [ " + viewModel.lastQrCode + " ]")
+
+            Button(action: {
+                viewModel.isShowing = true
+            }){
+                Text("カメラ起動")
+                Image(systemName: "camera")
+            }
+            .fullScreenCover(isPresented: $viewModel.isShowing) {
+                SecondView(viewModel: viewModel)
+            }
+        }
     }
 }
 
